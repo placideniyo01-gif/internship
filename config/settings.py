13b9251439af8ec4bdc64f6421a952b823ceed0e
placideniyo01-gif <placideniyo01@gmail.com>
2026-06-11 00:03:38 +0200

    debug mode

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jbml&s=o(+5@r9jerh!^#cb25g-_=04my1e9yzibcplb7d$vp='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
