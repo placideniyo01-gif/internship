b441b31a0308ebc16e83f6e5a6b3fa9032516961
placideniyo01-gif <placideniyo01@gmail.com>
2026-06-11 04:58:40 +0200

    migrate to postgres

--- config/settings.py
+++ config/settings.py
@@ -91,11 +91,12 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
