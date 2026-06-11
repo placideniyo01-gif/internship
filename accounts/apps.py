454915427dc555f774f911b241400e96e3c9a21f
placideniyo01-gif <placideniyo01@gmail.com>
2026-06-11 05:13:13 +0200

    auto create superuser

--- accounts/apps.py
+++ accounts/apps.py
@@ -7,3 +7,24 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         import accounts.signals
+
+        try:
+            from django.contrib.auth.models import User
+
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="byke04",
+                    email="placidebyke04@gmail.com",
+                    password="Placidebyke04@"
+                )
+                print("SUPERUSER CREATED")
+
+        except Exception as e:
+            print("SUPERUSER ERROR:", e)
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        import accounts.signals
